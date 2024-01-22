d2b33b2b77aebf26b9377c7eecbf490adb8f3a86
Nisarga-MS <84907841+Nisarga-MS@users.noreply.github.com>
2024-01-22 14:50:09 +0530

    added toggle between browse and homepage

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,17 +1,23 @@
-import React from "react";
-import BrowseMovies from "../BrowseMovies/BrowseMovies";
+import React from 'react'
+import BrowseMovies from '../BrowseMovies/BrowseMovies';
 import SmallProfil from "../../assets/images/profileSmall.png";
 import styles from "./Movies.module.css";
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Movies() {
+
+  const navigate = useNavigate();
+   const navitageToHome =()=>{
+    navigate("/")
+   }
+  
   return (
     <div className={styles.browse}>
       <div className={styles.browseHeader}>
         <div>
           <h3>Super app</h3>
-          <div>
-            <img src={SmallProfil} alt="smallprofilepick" />
-          </div>
+          <div onClick={navitageToHome}><img src={SmallProfil} alt ="smallprofilepick" /></div>
         </div>
         <h3>Entertainment according to your choice</h3>
       </div>
@@ -19,5 +25,5 @@ export default function Movies() {
         <BrowseMovies />
       </div>
     </div>
-  );
+  )
 }
